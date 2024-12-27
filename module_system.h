8e574ee3181381e0db7a13efa5e518e5355f8bf5
Izak8 <izak.baldacchino@outlook.com>
2024-12-28 00:31:23 +1030

    Add basic types

--- module_system.h
+++ module_system.h
@@ -1,20 +1,10 @@
-#ifndef MODULE_SYSTEM_TYPES_H
-#define MODULE_SYSTEM_TYPES_H
+#ifndef MODULE_SYSTEM_H
+#define MODULE_SYSTEM_H
 
 #include <stdint.h>
 
-/* Basic Types */
-
-/* Integers can be 64-bit, however network message values must be 32-bit */
+typedef char* ms_identifier;
 typedef uint32_t ms_int;
-typedef char* ms_string;
-typedef float ms_float;
-
-/* module_strings.py */
-typedef struct ms_strings_tuple {
-    ms_string id;
-    ms_string text;
-}ms_strings_tuple;
-
+typedef ms_int ms_index;
 
 #endif
