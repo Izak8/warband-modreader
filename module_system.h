cedbf5d6d0505c6980512a03db7b93febc9c291a
Izak8 <izak.baldacchino@outlook.com>
2024-12-28 00:31:53 +1030

    Add ms_string type
    Represents a string tuple from strings.txt

--- module_system.h
+++ module_system.h
@@ -7,4 +7,10 @@ typedef char* ms_identifier;
 typedef uint32_t ms_int;
 typedef ms_int ms_index;
 
+typedef struct ms_string {
+    ms_identifier identifier;
+    char* string;
+}ms_string;
+
+
 #endif
