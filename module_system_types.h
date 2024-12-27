ddd0467efb525149b651338242d67c0796ffa844
Izak8 <izak.baldacchino@outlook.com>
2024-12-27 21:58:39 +1030

    Add type ms_strings_tuple

--- module_system_types.h
+++ module_system_types.h
@@ -5,10 +5,16 @@
 
 /* Basic Types */
 
-/* Integers can be 64-bit, however when networked values must be 32-bit */
+/* Integers can be 64-bit, however network message values must be 32-bit */
 typedef uint32_t ms_int;
 typedef char* ms_string;
 typedef float ms_float;
+
+/* module_strings.py */
+typedef struct ms_strings_tuple {
+    ms_string id;
+    ms_string text;
+}ms_strings_tuple;
 
 
 #endif
