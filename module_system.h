dd3a68f423af87e59d36216c7b3f7395b4ed4a2a
Izak8 <izak.baldacchino@outlook.com>
2024-12-29 15:48:24 +1030

    Add ms_comp_trigger struct

--- module_system.h
+++ module_system.h
@@ -7,14 +7,15 @@
 typedef char* ms_identifier;
 typedef uint32_t ms_int;
 typedef ms_int ms_index;
+typedef float ms_trig_interval;
 
-/* String tuple */
+/* Strings */
 typedef struct ms_string {
     ms_identifier identifier;
     char* string;
 }ms_string;
 
-/* Script tuples */
+/* Scripts */
 
 /* TODO: figure out how this is to be decompiled */
 typedef size_t ms_comp_argument;
@@ -29,8 +30,33 @@ typedef struct ms_comp_statement {
 /* Compiled script tuple "<identifier> -1\n <length> <STATEMENT>*"*/
 typedef struct ms_comp_script {
     ms_identifier identifier;
-    ms_index length;
-    ms_statement* statements;
+    ms_index blocklen;
+    ms_statement* block;
 }ms_script;
+
+/* Simple Triggers */
+typedef struct ms_comp_simple_trigger {
+    ms_trig_interval interval;
+    ms_index blocklen;
+    ms_comp_statement* block;
+}ms_comp_simple_trigger;
+
+/* Regular Triggers */
+
+/* rearm_interval can take a value 100000000.000000 which translates to constant ti_once */
+#define TI_ONCE 100000000.000000
+
+typedef struct ms_comp_trigger {
+    ms_trig_interval check_interval;
+    ms_trig_interval delay_interval;
+    ms_trig_interval rearm_interval;
+
+    ms_index blocklen_cond;
+    ms_comp_statement* condblock;
+
+    ms_index blocklen_cons;
+    ms_comp_statement* consblock;
+
+}ms_comp_trigger;
 
 #endif
